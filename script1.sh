c6561bff496f65278d4fc99a738c5b644a87dc60
hire72149 <hirengondaliya275@gmail.com>
2023-05-12 06:20:08 +0000

    Adding script1.sh

---
+++ script1.sh
@@ -0,0 +1,11 @@
+echo "*********************************Begin script execution***********************************"
+mkdir script
+cd script
+touch file1.txt > This is file1.
+touch file2.txt > This is file2.
+echo "*********************************Display files*******************************************"
+ls
+echo "********************************Display files content************************************"
+cat file1.txt
+cat file2.txt
+echo "**********************************End script**********************************************"
