a7278c5debd1090e0a4bc8136bdafc6985b43866
hire72149 <hirengondaliya275@gmail.com>
2023-05-12 09:31:34 +0000

    Updated script1

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 echo "*********************************Begin script execution***********************************"
 mkdir script
 cd script
-touch file1.txt > This is file1.
-touch file2.txt > This is file2.
+touch file1.txt > "This is file1."
+touch file2.txt > "This is file2."
 echo "*********************************Display files*******************************************"
 ls
 echo "********************************Display files content************************************"
