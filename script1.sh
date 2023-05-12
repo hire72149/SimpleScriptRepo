4b6b00cbcc88fbb34956ff2fd818427caa1804a3
hire72149 <hirengondaliya275@gmail.com>
2023-05-12 09:37:47 +0000

    Updated script1

--- script1.sh
+++ script1.sh
@@ -3,7 +3,6 @@ mkdir script
 cd script
 touch file1.txt > "This is file1."
 touch file2.txt > "This is file2."
-echo "*********************************Display files*******************************************"
 ls
 echo "********************************Display files content************************************"
 cat file1.txt
